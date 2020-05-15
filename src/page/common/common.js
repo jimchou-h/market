8d8dea42197e3e31b548ff2138419d29a8f4ba1f
unknown <1249293339@qq.com>
2020-05-15 13:48:43 +0800

    first

---
+++ src/page/common/common.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+require('./normalize.css'); //CSS reset
+require('./layout.css');
+require('node_modules/font-awesome/css/font-awesome.min.css'); //字体文件
+require('./footer/index.css')
